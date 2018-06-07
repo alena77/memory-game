f3cde6ea0e05a903d55341c55f8779f3e360f4a4
Alena Yevdokimova <alyona.y3@gmail.com>
2018-06-07 18:22:35 -0400

    added event listener for cards

--- js/app.js
+++ js/app.js
@@ -3,66 +3,71 @@ document.addEventListener('DOMContentLoaded', function () {
       
 
 
-//array definition with all cards
-var cards = [
-'<li class="card open show"><i class="fa fa-diamond"></i></li>',
-'<li class="card open show"><i class="fa fa-paper-plane-o"></i></li>',
-'<li class="card match"><i class="fa fa-anchor"></i></li>',
-'<li class="card open show"><i class="fa fa-bolt"></i></li>',
-'<li class="card open show"><i class="fa fa-cube"></i></li>',
-'<li class="card match"><i class="fa fa-anchor"></i></li>',
-'<li class="card open show"><i class="fa fa-leaf"></i></li>',
-'<li class="card open show"><i class="fa fa-bicycle"></i></li>',
-'<li class="card open show"><i class="fa fa-diamond"></i></li>',
-'<li class="card open show"><i class="fa fa-bomb"></i></li>',
-'<li class="card open show"><i class="fa fa-leaf"></i></li>',
-'<li class="card open show"><i class="fa fa-bomb"></i></li>',
-'<li class="card open show"><i class="fa fa-bolt"></i></li>',
-'<li class="card open show"><i class="fa fa-bicycle"></i></li>',
-'<li class="card open show"><i class="fa fa-paper-plane-o"></i></li>',
-'<li class="card open show"><i class="fa fa-cube"></i></li>'
-];
+	//array definition with all cards
+	var cards = [
+	'<li class="card open show"><i class="fa fa-diamond"></i></li>',
+	'<li class="card open show"><i class="fa fa-paper-plane-o"></i></li>',
+	'<li class="card match"><i class="fa fa-anchor"></i></li>',
+	'<li class="card open show"><i class="fa fa-bolt"></i></li>',
+	'<li class="card open show"><i class="fa fa-cube"></i></li>',
+	'<li class="card match"><i class="fa fa-anchor"></i></li>',
+	'<li class="card open show"><i class="fa fa-leaf"></i></li>',
+	'<li class="card open show"><i class="fa fa-bicycle"></i></li>',
+	'<li class="card open show"><i class="fa fa-diamond"></i></li>',
+	'<li class="card open show"><i class="fa fa-bomb"></i></li>',
+	'<li class="card open show"><i class="fa fa-leaf"></i></li>',
+	'<li class="card open show"><i class="fa fa-bomb"></i></li>',
+	'<li class="card open show"><i class="fa fa-bolt"></i></li>',
+	'<li class="card open show"><i class="fa fa-bicycle"></i></li>',
+	'<li class="card open show"><i class="fa fa-paper-plane-o"></i></li>',
+	'<li class="card open show"><i class="fa fa-cube"></i></li>'
+	];
 
-//initial card shuffle at page load
-shuffle(cards);
+	//initial card shuffle at page load
+	shuffle(cards);
 
-console.log(document.getElementsByClassName("deck"));
-//display initial shuffled cards on page
-document.getElementsByClassName("deck")[0].innerHTML = cards.join("");
-
-
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- var el = document.querySelectorAll('.card');
-console.log(el);
-document.addEventListener("click", function(){ alert("Hello World!"); }); 
- */
+	console.log(document.getElementsByClassName("deck"));
+	//display initial shuffled cards on page
+	document.getElementsByClassName("deck")[0].innerHTML = cards.join("");
 
 
 
+	// Shuffle function from http://stackoverflow.com/a/2450976
+	function shuffle(array) {
+	    var currentIndex = array.length, temporaryValue, randomIndex;
 
+	    while (currentIndex !== 0) {
+	        randomIndex = Math.floor(Math.random() * currentIndex);
+	        currentIndex -= 1;
+	        temporaryValue = array[currentIndex];
+	        array[currentIndex] = array[randomIndex];
+	        array[randomIndex] = temporaryValue;
+	    }
+
+	    return array;
+	}
+
+
+	/*
+	 * set up the event listener for a card. If a card is clicked:
+	 *  - display the card's symbol (put this functionality in another function that you call from this one)
+	 *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+	 *  - if the list already has another card, check to see if the two cards match
+	 *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+	 *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+	 *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+	 *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one) 
+	 */
+
+	//event listener for cards
+	var cardEl = document.querySelector('.deck');
+	console.log(cardEl);
+	cardEl.addEventListener("click", respondToTheClick);
+
+	//function called when card is clicked
+	function respondToTheClick(evt) {
+		if (evt.target.nodeName === 'LI') {  // ← verifies target is desired element
+        	console.log('A cardEl was clicked: ' + evt.target.textContent);
+    	}
+	}
 });
